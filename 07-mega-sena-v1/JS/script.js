61e6ab0279fdf4b199f483d7f6f75ed8dcc351be
marcelobentes <74937617+marcelobentes@users.noreply.github.com>
2022-08-17 22:26:07 -0300

    adicionando a função de adicionar numero

--- 07-mega-sena-v1/JS/script.js
+++ 07-mega-sena-v1/JS/script.js
@@ -1,1 +1,33 @@
-console.log('Testando o javaScript!');
+var state = {
+  board: [],
+  currentGame: [],
+  savedGames: [],
+};
+
+function start() {};
+
+<!--função de adicionar numero -->
+function addNumberToGame(numberToAdd){
+  if(numberToAdd < 1 || numberToAdd > 60){
+    console.error('Numero inválido', numberToAdd);
+    return;
+  }
+  if (state.currentGame.length >=6){
+    console.error('O jogo ja esta completo.');
+    return;
+  }
+  if (isNumberInGame(numberToAdd)){
+    console.error('Este numero ja esta no jogo.', numberToAdd);
+  }
+
+  state.currentGame.push(numberToAdd);
+};
+
+function isNumberInGame(numberToAdd){
+  if(state.currentGame.includes(numberToAdd)){
+    return true;
+  }
+  return false;
+};
+
+start();
