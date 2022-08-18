5193429faf0ab87cebf47b1e24db577ed87bd4ec
marcelobentes <74937617+marcelobentes@users.noreply.github.com>
2022-08-17 21:49:15 -0300

    finalizado projeto IMC

--- 06-imc/JS/script.js
+++ 06-imc/JS/script.js
@@ -1,1 +1,23 @@
-console.log('Testando o javaScript!');
+function start() {
+  var buttonCalcular = document.querySelector('#button-calcular-imc');
+  buttonCalcular.addEventListener('click', buttonClick);
+
+  buttonClick();
+}
+
+function calcularImc(peso, altura) {
+  return peso / (altura * altura);
+}
+
+function buttonClick() {
+  let inputWeight = document.querySelector('#input-weight');
+  let inputHeight = document.querySelector('#input-height');
+  let imcResult = document.querySelector('#imc-result');
+  var weight = Number(inputWeight.value);
+  var height = Number(inputHeight.value);
+  let imc = calcularImc(weight, height);
+  let formatedImc = imc.toFixed(2).replace('.', ',');
+  imcResult.textContent = formatedImc;
+}
+
+start();
