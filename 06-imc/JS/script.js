a88d700f08032ecb835580de058ea373e88169cc
marcelobentes <marcelo.maia.m@gmail.com>
2022-08-22 17:44:49 -0300

    imc-v2

--- 06-imc/JS/script.js
+++ 06-imc/JS/script.js
@@ -13,11 +13,18 @@ function buttonClick() {
   let inputWeight = document.querySelector('#input-weight');
   let inputHeight = document.querySelector('#input-height');
   let imcResult = document.querySelector('#imc-result');
+  let faixaPesoResult = document.querySelector('#faixa-peso');
   var weight = Number(inputWeight.value);
   var height = Number(inputHeight.value);
   let imc = calcularImc(weight, height);
-  let formatedImc = imc.toFixed(2).replace('.', ',');
+  var formatedImc = imc.toFixed(2).replace('.', ',');
   imcResult.textContent = formatedImc;
+
+  if (formatedImc > 16.0 && formatedImc < 17.0) {
+    faixaPesoResult.textContent = 'Muito abaixo do peso.';
+  } else {
+    faixaPesoResult.textContent = 'não entrou no if';
+  }
 }
 
 start();
